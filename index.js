2b3c5f4f78bc815972675ee9b68238c25fa81763
Xasanov Ibrohim <xasanovibrohim555@gmail.com>
2024-01-02 15:34:23 +0500

    added charts

---
+++ index.js
@@ -0,0 +1,49 @@
+const settingsIcon = document.querySelector('.settings');
+const settingsModal = document.querySelector('.settings-modal');
+
+settingsIcon.addEventListener('click', () => {
+  // Show settings modal
+  settingsModal.classList.toggle('hidden');
+});
+
+// Your data
+const maleCount = 30;
+const femaleCount = 50;
+const others = 2;
+
+// Create a pie chart
+const gender = document.getElementById('genderChart').getContext('2d');
+const genderChart = new Chart(gender, {
+  type: 'pie',
+  data: {
+    labels: ['Male', 'Female',"Othersssssss"],
+    datasets: [
+      {
+        data: [maleCount, femaleCount,others],
+        backgroundColor: ['#E21A1A', '#509ADB','#9228aa'],
+      },
+    ],
+  },
+});
+
+// Your data
+const accounting = 30;
+const IT = 50;
+const marketing = 50;
+const departments = document
+  .getElementById('departmentsChart')
+  .getContext('2d');
+const departmentsChart = new Chart(departments, {
+  type: 'pie',
+  data: {
+    labels: ['Accounting', 'IT', 'Marketing'],
+    datasets: [
+      {
+        data: [accounting, IT, marketing],
+        backgroundColor: ['#9228aa', 'yellow', '#2ecc71'],
+      },
+    ],
+  },
+});
+
+
